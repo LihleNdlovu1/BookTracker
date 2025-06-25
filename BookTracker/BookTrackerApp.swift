0c985de95267ccadf6453939d6ac6471f04813bb
LihleNdlovu1 <lihlendlovu@glucode.com>
2025-06-25 12:50:58 +0200

    Initial Commit

---
+++ BookTracker/BookTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTrackerApp.swift
+//  BookTracker
+//
+//  Created by Lubelihle Ndlovu on 2025/06/25.
+//
+
+import SwiftUI
+
+@main
+struct BookTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookTrackerTests/BookTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  BookTrackerTests.swift
+//  BookTrackerTests
+//
+//  Created by Lubelihle Ndlovu on 2025/06/25.
+//
+
+import Testing
+
+struct BookTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
