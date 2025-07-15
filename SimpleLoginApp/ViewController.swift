5bd6359468d2f76e8b5f3efd68e3b6c9fa09048b
LihleNdlovu1 <lihlendlovu@glucode.com>
2025-07-15 10:00:39 +0200

    Initial Commit

---
+++ SimpleLoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleLoginApp
+//
+//  Created by Lubelihle Ndlovu on 2025/07/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
